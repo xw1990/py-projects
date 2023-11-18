d1d7d695dd43bf06b7ab6c08f005f8c7732c595f
xw1990 <957879717@qq.com>
2023-11-18 23:14:32 +0800

    LeetCode20

---
+++ LeetCode20.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic: stack.append(c)
+            elif dic[stack.pop()] != c: return False 
+        return len(stack) == 1
+
+test = Solution()    
+print("result:",test.isValid('[][][][]}[][]'))
