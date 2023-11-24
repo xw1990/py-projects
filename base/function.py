9fac1f7b0f4f415001dabde6e6a4dd74d82fc350
xw1990 <957879717@qq.com>
2023-11-24 09:04:44 +0800

    1

--- base/function.py
+++ base/function.py
@@ -39,4 +39,32 @@ def printinfo( arg1, *vartuple ):
 # 调用printinfo 函数
 printinfo( 70, 60, 50 )
 
+print ("------------------------")
+
+#加了两个星号 ** 的参数会以字典的形式导入
+# 可写函数说明
+def printinfo( arg1, **vardict ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   print (vardict)
+ 
+# 调用printinfo 函数
+printinfo(1, a=2,b=3)
 print ("==============================")
+
+# 匿名函数
+x = lambda a : a + 10
+print(x(5))
+
+def myfunc(n):
+  return lambda a : a * n
+ 
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+ 
+print(mydoubler(11))
+print(mytripler(11))
+
+print ("==============================")
+print(dir())
